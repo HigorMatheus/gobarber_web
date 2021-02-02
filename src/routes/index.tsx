e6e816885fc275e9d9c4bd9651f8180e53b9e19b
Higor Matheus O Vieira <hmoliveira83@gmail.com>
2021-02-01 21:40:18 -0300

    produ config

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -18,8 +18,8 @@ const Routes: React.FC = () => {
       <Route path="/forgot-password" component={ForgotPassword} />
       <Route path="/reset-password" component={ResetPassword} />
 
-      <Route path="/profile" component={Profile} isPrivate />
-      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile}  />
+      <Route path="/dashboard" component={Dashboard}  />
     </Switch>
   );
 };
